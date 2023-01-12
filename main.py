f2f6f45f3936c229aa46e83ed5d80f60e53129f8
albertomerango <alberto.marengo@coeuraj.com>
2023-01-12 15:43:51 -0800

    SageMaker implementation - Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import torchvision
+from torchvision.utils import make_grid
+from torchvision import datasets, transforms
+from src.model import MLModel
+from src.trainer import Trainer
+from src.dataloader import Loader
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
